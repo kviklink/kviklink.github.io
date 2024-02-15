231270604bffff3e5ed2c03b06591477791f33d9
biwecka <biwecka@mailbox.org>
2024-02-15 12:27:07 +0100

    move from 'jest' to 'vitest' because of ESM incompatibilities

--- libs/xbs/src/builder/builder.spec.ts
+++ libs/xbs/src/builder/builder.spec.ts
@@ -1,9 +1,20 @@
 // Imports /////////////////////////////////////////////////////////////////////
+import { describe, it, expect } from 'vitest'
+import { XbsBuilder } from './builder'
 
 // Tests ///////////////////////////////////////////////////////////////////////
 describe('builder', () => {
-    it('should work', () => {
-        expect(1).toBe(1)
+    it('should return error', async () => {
+        const xbs = await new XbsBuilder().finish()
+        expect(xbs.err).toBe(true)
+    })
+
+    it('should return ok', async () => {
+        const xbs = await new XbsBuilder()
+            .setCredentials('asdf', '123')
+            .finish()
+
+        expect(xbs.ok).toBe(true)
     })
 })
 
