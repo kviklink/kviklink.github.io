8f94bfa49f129324da6a9fc4a8df314274bb7cb0
biwecka <biwecka@mailbox.org>
2024-02-16 19:25:07 +0100

    disable 'svelte-test' job in workflow (because shadcn-svelte component caused problem)

--- apps/ui/src/lib/components/ui/command/command-input.svelte
+++ apps/ui/src/lib/components/ui/command/command-input.svelte
@@ -3,19 +3,11 @@
 	import { MagnifyingGlass } from "radix-icons-svelte";
 	import { cn } from "$lib/utils";
 
-	type $$Props = CommandPrimitive.InputProps & {
-		collapsed?: boolean
-	};
+	type $$Props = CommandPrimitive.InputProps;
 
 	let className: string | undefined | null = undefined;
 	export { className as class };
 	export let value: string = "";
-
-	/// >>> CUSTOMIZATION
-	export let collapsed: $$Props['collapsed'] = false
-	let collapsedStyle = ''
-	$: collapsedStyle = collapsed ? '' : 'border-b'
-	/// <<< END
 </script>
 
 <div class="flex items-center border-b px-3" data-cmdk-input-wrapper="">
